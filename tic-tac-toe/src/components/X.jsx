b982de2948f526cbfec274f180aa86ec127eb717
Ishaak <Ishaak019@gmail.com>
2026-07-05 19:15:54 +0200

    created x component

---
+++ tic-tac-toe/src/components/X.jsx
@@ -0,0 +1,13 @@
+import "./X.css"
+
+export default function X() {
+
+    return (
+        
+        <div className="line1">
+            <div className="line2"  />
+        </div>
+    
+    )
+
+}
