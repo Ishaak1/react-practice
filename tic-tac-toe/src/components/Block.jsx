4874462d788f1edfa849014c2b0ecb2a4a16991b
Ishaak <Ishaak019@gmail.com>
2026-07-05 19:12:56 +0200

    created block component

---
+++ tic-tac-toe/src/components/Block.jsx
@@ -0,0 +1,23 @@
+import "./Block.css"
+import O from "./O"
+import X from "./X"
+
+export default function Block({ symbol, id, onClick }) {
+
+    function selected() {
+
+        onClick(id)
+
+    }
+
+    return (
+
+        <div className="block" onClick={selected}>
+
+            {(symbol === "-") ? null : ((symbol === "x") ? <X /> : <O /> )}
+
+        </div>
+
+    )
+
+}
