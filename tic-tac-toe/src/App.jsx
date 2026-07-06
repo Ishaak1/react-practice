3f7d2ea0126f9e2acd7553e2426c51983d5fc90e
Ishaak <Ishaak019@gmail.com>
2026-07-06 13:14:42 +0200

    implemented game logic

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,6 +1,11 @@
 import Board from "./components/Board"
 import Row from "./components/Row"
 import Block from "./components/Block"
+import Banner from "./components/Banner" 
+import ScoreBoard from "./components/ScoreBoard"
+import InputResetSection from "./components/InputResetSection"
+import ResetButton from "./components/ResetButton"
+import PlayAgainButton from "./components/PlayAgainButton"
 import { useState } from "react"
 import './App.css'
 
@@ -12,6 +17,7 @@ export default function App() {
   const [playerName, setPlayerName] = useState("YOU")
   const [playerScore, setPlayerScore] = useState(0)
   const [cpuScore, setCpuScore] = useState(0)
+  const [input, setInput] = useState("")
 
   function play(id) {
 
@@ -34,10 +40,9 @@ export default function App() {
         
         const newScore = playerScore + 1
         setPlayerScore(newScore)
+
         const newWinner = playerName
         setWinnerName(newWinner)
-        console.log(playerName)
-        console.log(newScore)
         
       } else {
       
@@ -60,10 +65,9 @@ export default function App() {
         
         const newScore = cpuScore + 1
         setCpuScore(newScore)
+
         const newWinner = "CPU"
         setWinnerName(newWinner)
-        console.log("CPU")
-        console.log(newScore)
 
       }
       
@@ -74,14 +78,77 @@ export default function App() {
 
   }
 
+  function resetGame() {
+
+    setBoard(["-", "-", "-", "-", "-", "-", "-", "-", "-"])
+    setRemaining([0, 1, 2, 3, 4, 5, 6, 7, 8])
+    setWinnerName("")
+    setPlayerName("YOU")
+    setPlayerScore(0)
+    setCpuScore(0)
+    
+  }
+
+  function updateName() {
+
+    setPlayerName(input)
+    setInput("")
+
+  }
+
+  function playAgain() {
+
+    setBoard(["-", "-", "-", "-", "-", "-", "-", "-", "-"])
+    setRemaining([0, 1, 2, 3, 4, 5, 6, 7, 8])
+    setWinnerName("")
+
+  }
+
   return (
     
     <>
 
       <div className="background">
 
+        <div className="leftSpace"/>
+
         <Board board={board}
                onClick={play} />
+
+        <div className="right">
+
+          <div className="sideBar">
+
+            {
+
+              ((winnerName !== "") || (remaining.length === 1)) && <Banner name={winnerName} />
+            
+            }
+
+            {
+            
+              (cpuScore !== 0 || playerScore !== 0) && (<ScoreBoard name={playerName}
+                                                                    cpuScore={cpuScore}
+                                                                    playerScore={playerScore}/>)
+            
+            }
+
+            <InputResetSection input={input}
+                               updateName={updateName}
+                               resetGame={resetGame}
+                               setInput={setInput}
+                               cpuScore={cpuScore}
+                               playerScore={playerScore}/>
+
+            {
+           
+              (winnerName !== "" || remaining.length === 1) && <PlayAgainButton playAgain={playAgain} />
+           
+            }            
+
+          </div>  
+
+        </div>
 
       </div>
 
