81fe60889a1f336e044cfd1e1c06a9daa70883ef
Ishaak <Ishaak019@gmail.com>
2026-07-13 11:29:34 +0200

    created no tasks component

---
+++ to-do-list/src/components/NoTasks.jsx
@@ -0,0 +1,15 @@
+import "./NoTasks.css"
+
+export default function NoTasks() {
+
+    return (
+
+        <div className="noTasks">
+
+            <p className="noTasksText">SUBMIT NEW TASKS TO VIEW THEM HERE</p>
+
+        </div>
+
+    )
+
+}
