bfbc7cdf050dc34074ff15ce648ecfd1cd6b80fc
Ishaak <Ishaak019@gmail.com>
2026-07-13 11:34:58 +0200

    implemented to do list app

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,122 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import TaskDeleteRow from "./components/TaskDeleteRow"
+import NoTasks from "./components/NoTasks"
+import InputSubmitSection from "./components/InputSubmitSection"
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
+  const [tasks, setTasks] = useState([])
+  const [input, setInput] = useState("")
+  const [keys, setKeys] = useState([])
+
+  function submit() {
+
+    const newInput = input
+    const newTasks = structuredClone(tasks)
+    newTasks.push(newInput)
+    setTasks(newTasks)
+    setInput("")
+
+    var newKeys = structuredClone(keys)
+    newKeys.push(crypto.randomUUID())
+    setKeys(newKeys)
+
+  }
+
+  function removeTask(id) {
+
+    var newTasks = structuredClone(tasks)
+    newTasks = newTasks.filter((item, index) => index !== id)
+    setTasks(newTasks)    
+
+    var newKeys = structuredClone(keys)
+    newKeys = newKeys.filter((item, index) => index !== id)
+    setKeys(newKeys)
+
+  }
 
   return (
+
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
 
-      <div className="ticks"></div>
+      <div className="background">
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        <div className="placeholder" />
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+        <div className="container">
+          
+          <div className="cards">          
+          
+          {
+            
+            (tasks.length === 0) ?  <NoTasks /> : tasks.map((item, index) => <TaskDeleteRow row={index}
+                                                                                            task={item}
+                                                                                            key={keys[index]}
+                                                                                            lastTask={tasks.length - 1}
+                                                                                            onClick={removeTask} />) 
+            
+          }
+          
+          </div>
+
+          <InputSubmitSection input={input} 
+                              setInput={setInput}
+                              onClick={submit} />
+
+        </div>
+      
+      </div>
+
     </>
-  )
-}
 
-export default App
+  )
+
+}
